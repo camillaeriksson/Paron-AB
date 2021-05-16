813793dadd366de2e6d748bb25fa809157aff92d
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-16 18:03:33 +0200

    Installed axios

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -3,8 +3,15 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
   name: 'stock',
+  methods: {
+    getAllProducts() {
+      axios.get('http://localhost:8081/products')
+      .then(response => (this.products = response.data))
+    }
+  }
 }
 </script>
 
