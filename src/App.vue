f4e53335e96a6bcc86bf927f53c50f26e5c7ee5b
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-16 16:23:37 +0200

    Made webmenu component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <router-view/>
+  <WebMenu />
+  <router-view />
 </template>
 
 <script>
+import WebMenu from './components/WebMenu.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    WebMenu
+  }
 }
 </script>
 
@@ -14,8 +19,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: row;
 }
 </style>
---
+++ src/components/WebMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="web-menu-container">
+    <p>Web Menu</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebMenu',
+}
+</script>
+
+<style lang="scss">
+.web-menu-container {
+  height: 100vh;
+  width: 20vw;
+  background: red;
+}
+
+</style>
