996195326aa7aea0868eba7a38018635fd1fcddf
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-16 13:01:35 +0200

    Installed vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RouterViewContainer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RouterViewContainer from './RouterViewContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RouterViewContainer
   }
 }
 </script>
