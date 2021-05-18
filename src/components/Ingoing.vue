efcb08ed0f49e493266a7875fe4f1754e1a98776
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-18 19:17:27 +0200

    Made dropdown for warehouses in ingoing

--- src/components/Ingoing.vue
+++ src/components/Ingoing.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
-  <select v-model="selected">
-    <option disabled value="">Please select one</option>
-    <option>Cupertino</option>
-    <option>Norrköping</option>
-    <option>Frankurt</option>
-  </select>
+    <Dropdown v-model="selectedWarehouse" :options="warehouses" optionLabel="name" optionValue="value" placeholder="Välj ett lager" />
     <button @click="submit()">Submit</button>
   </div>
 </template>
 
 <script>
+import Dropdown from 'primevue/dropdown'
 export default {
   name: 'ingoing',
   data() {
-    return {
-      selected: ''
-    }
-  },
+	return {
+		selectedWarehouse: null,
+		warehouses: [
+			{name: 'Cupertino', value: 'cupertino'},
+			{name: 'Norrköping', value: 'norrkoping'},
+			{name: 'Frankurt', value: 'frankurt'},
+		]
+	}
+},
+components: {
+  Dropdown
+},
   methods: {
     submit() {
-      console.log(this.message)
+      console.log(this.selectedWarehouse)
     }
   }
 }
