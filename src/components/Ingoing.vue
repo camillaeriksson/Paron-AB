8eb7cb52972f1160b0de295fcf7f0182f1ad920a
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-18 19:20:42 +0200

    Made a dropdown with products in ingoing

--- src/components/Ingoing.vue
+++ src/components/Ingoing.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Dropdown v-model="selectedProduct" :options="products" optionLabel="name" optionValue="value" placeholder="Välj en produkt" />
     <Dropdown v-model="selectedWarehouse" :options="warehouses" optionLabel="name" optionValue="value" placeholder="Välj ett lager" />
     <button @click="submit()">Submit</button>
   </div>
@@ -11,11 +12,17 @@ export default {
   name: 'ingoing',
   data() {
 	return {
+    selectedProduct: null,
 		selectedWarehouse: null,
 		warehouses: [
 			{name: 'Cupertino', value: 'cupertino'},
 			{name: 'Norrköping', value: 'norrkoping'},
 			{name: 'Frankurt', value: 'frankurt'},
+		],
+    products: [
+			{name: 'jTelefon', value: 1},
+			{name: 'jPlatta', value: 2},
+			{name: 'Päronklocka', value: 3},
 		]
 	}
 },
