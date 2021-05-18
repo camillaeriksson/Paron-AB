05dd0204e07a556bd9ca7fde56cef19b085222bd
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-18 19:31:11 +0200

    Added input field for quantity in ingoing

--- src/components/Ingoing.vue
+++ src/components/Ingoing.vue
@@ -2,36 +2,45 @@
   <div>
     <Dropdown v-model="selectedProduct" :options="products" optionLabel="name" optionValue="value" placeholder="Välj en produkt" />
     <Dropdown v-model="selectedWarehouse" :options="warehouses" optionLabel="name" optionValue="value" placeholder="Välj ett lager" />
-    <button @click="submit()">Submit</button>
+    <span class="p-float-label">
+      <InputText id="quantity" type="number" v-model="quantity" />
+      <label for="quantity">Antal</label>
+    </span>
+    <button @click="submit()">Skicka</button>
   </div>
 </template>
 
 <script>
 import Dropdown from 'primevue/dropdown'
+import InputText from 'primevue/inputtext'
+
 export default {
   name: 'ingoing',
   data() {
 	return {
+    quantity: null,
     selectedProduct: null,
 		selectedWarehouse: null,
 		warehouses: [
 			{name: 'Cupertino', value: 'cupertino'},
 			{name: 'Norrköping', value: 'norrkoping'},
-			{name: 'Frankurt', value: 'frankurt'},
+			{name: 'Frankurt', value: 'frankurt'}
 		],
     products: [
 			{name: 'jTelefon', value: 1},
 			{name: 'jPlatta', value: 2},
-			{name: 'Päronklocka', value: 3},
+			{name: 'Päronklocka', value: 3}
 		]
 	}
 },
 components: {
-  Dropdown
+  Dropdown,
+  InputText
 },
   methods: {
     submit() {
       console.log(this.selectedWarehouse)
+      console.log(this.selectedProduct)
     }
   }
 }
