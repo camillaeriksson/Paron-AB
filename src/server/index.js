674d01fde0cb6235372280ea45b7567c1ad5f0fc
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-18 17:43:46 +0200

    Made comments on the code in the index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,20 @@
+// Setting up the express app and port
 const express = require("express")
 const app = express()
 const port = 8081
 const fs = require("fs")
 const cors = require('cors')
 
+// Parsing incoming requests with JSON payloads, using cors (cross-origin resource sharing) 
 app.use(express.json())
-app.use(express.static("public"))
 app.use(cors());
 
+// Add stock to the chosen product and warehouse
 app.patch("/products/:id/ingoing", (req, res) => {
-  console.log('jsfkja')
   const productData = fs.readFileSync("products.json");
   let products = JSON.parse(productData);
   const newQuantity = req.body.ingoingQuantity
   const warehouse = req.body.warehouse
-  // const ingoingQuantity = req.body.ingoingQuantity;
   const productIndex = products.findIndex(
     (product) => product.id === parseInt(req.params.id)
   );
@@ -24,16 +24,16 @@ app.patch("/products/:id/ingoing", (req, res) => {
     products[productIndex].stock[warehouse] += newQuantity
   }
   res.send();
-  fs.writeFileSync("products.json", JSON.stringify(products));
+  fs.writeFileSync("products.json", JSON.stringify(products))
 });
 
-app.get("/", (req, res) => res.send("Welcome!"))
-
+// Get all products 
 app.get("/products", (req, res) => {
   const productData = fs.readFileSync("products.json")
   const products = JSON.parse(productData)
   res.send(products)
 });
 
+app.get("/", (req, res) => res.send("Welcome!"))
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
