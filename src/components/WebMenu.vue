198d18359d0ab7450dc98c62a3a038226077efef
Camilla Eriksson <ericsson.camilla@gmail.com>
2021-05-20 18:38:15 +0200

    Smaller logo in topmenu and icons

--- src/components/WebMenu.vue
+++ src/components/WebMenu.vue
@@ -1,15 +1,19 @@
 <template>
-<div>
-<Menubar :model="items" class="top-menu"/>
-  <div class="side-menu-container">
-    <img src="../assets/logo.svg">
-    <div class="router-links-container">
-      <router-link class="router-link" to="/stock"><i class="fas fa-lg fa-cubes"></i>Lagersaldo</router-link>
-      <router-link class="router-link" to="/ingoing"><i class="fas fa-lg fa-sign-in-alt"></i>Ingående</router-link>
-      <router-link class="router-link" to="/outgoing"><i class="fas fa-lg fa-sign-out-alt"></i>Utgående</router-link>
+  <div>
+    <Menubar :model="items" class="top-menu">
+      <template #end>
+        <img src="../assets/small_logo.svg">
+      </template>
+    </Menubar>
+    <div class="side-menu-container">
+      <img src="../assets/logo.svg">
+      <div class="router-links-container">
+        <router-link class="router-link" to="/stock"><i class="fas fa-lg fa-cubes"></i>Lagersaldo</router-link>
+        <router-link class="router-link" to="/ingoing"><i class="fas fa-lg fa-sign-in-alt"></i>Ingående</router-link>
+        <router-link class="router-link" to="/outgoing"><i class="fas fa-lg fa-sign-out-alt"></i>Utgående</router-link>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -17,140 +21,29 @@ import Menubar from 'primevue/menubar'
 export default {
   name: 'WebMenu',
   data() {
-        return {
-            items: [
-                {
-                   label:'File',
-                   icon:'pi pi-fw pi-file',
-                   items:[
-                      {
-                         label:'New',
-                         icon:'pi pi-fw pi-plus',
-                         items:[
-                            {
-                               label:'Bookmark',
-                               icon:'pi pi-fw pi-bookmark'
-                            },
-                            {
-                               label:'Video',
-                               icon:'pi pi-fw pi-video'
-                            },
-
-                         ]
-                      },
-                      {
-                         label:'Delete',
-                         icon:'pi pi-fw pi-trash'
-                      },
-                      {
-                         separator:true
-                      },
-                      {
-                         label:'Export',
-                         icon:'pi pi-fw pi-external-link'
-                      }
-                   ]
-                },
-                {
-                   label:'Edit',
-                   icon:'pi pi-fw pi-pencil',
-                   items:[
-                      {
-                         label:'Left',
-                         icon:'pi pi-fw pi-align-left'
-                      },
-                      {
-                         label:'Right',
-                         icon:'pi pi-fw pi-align-right'
-                      },
-                      {
-                         label:'Center',
-                         icon:'pi pi-fw pi-align-center'
-                      },
-                      {
-                         label:'Justify',
-                         icon:'pi pi-fw pi-align-justify'
-                      },
-
-                   ]
-                },
-                {
-                   label:'Users',
-                   icon:'pi pi-fw pi-user',
-                   items:[
-                      {
-                         label:'New',
-                         icon:'pi pi-fw pi-user-plus',
-
-                      },
-                      {
-                         label:'Delete',
-                         icon:'pi pi-fw pi-user-minus',
-
-                      },
-                      {
-                         label:'Search',
-                         icon:'pi pi-fw pi-users',
-                         items:[
-                            {
-                               label:'Filter',
-                               icon:'pi pi-fw pi-filter',
-                               items:[
-                                  {
-                                     label:'Print',
-                                     icon:'pi pi-fw pi-print'
-                                  }
-                               ]
-                            },
-                            {
-                               icon:'pi pi-fw pi-bars',
-                               label:'List'
-                            }
-                         ]
-                      }
-                   ]
-                },
-                {
-                   label:'Events',
-                   icon:'pi pi-fw pi-calendar',
-                   items:[
-                      {
-                         label:'Edit',
-                         icon:'pi pi-fw pi-pencil',
-                         items:[
-                            {
-                               label:'Save',
-                               icon:'pi pi-fw pi-calendar-plus'
-                            },
-                            {
-                               label:'Delete',
-                               icon:'pi pi-fw pi-calendar-minus'
-                            },
-
-                         ]
-                      },
-                      {
-                         label:'Archieve',
-                         icon:'pi pi-fw pi-calendar-times',
-                         items:[
-                            {
-                               label:'Remove',
-                               icon:'pi pi-fw pi-calendar-minus'
-                            }
-                         ]
-                      }
-                   ]
-                },
-                {
-                   label:'Quit',
-                   icon:'pi pi-fw pi-power-off'
-                }
-             ]
-        }
-    },
-    components: {
-      Menubar
+    return {
+      items: [
+          {
+            label:'Lagersaldo',
+            icon:'fas fa-lg fa-cubes',
+            url: '/stock'
+          },
+          {
+            label:'Ingående',
+            icon:'fas fa-lg fa-sign-in-alt',
+            url: '/ingoing'
+          },
+          {
+            label:'Utgående',
+            icon:'fas fa-lg fa-sign-out-alt',
+            url: '/outgoing'
+          }
+      ]
     }
+  },
+  components: {
+    Menubar
+  }
 }
 </script>
 
